4915205ff13fe9da755952931f007c7ef7cc4c1e
doxbin07-eng <doxbin07@gmail.com>
2026-05-05 01:38:04 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 # settings.py
-BOT_TOKEN = "8550356522:AAFViUadFM3kcldMtIhwD_rkCk96jsafOqI"  # Замените на ваш токен
+BOT_TOKEN = "8550356522:AAEvWF2zclndI1pWvl4zG9S1oz8L_LJY1yk"  # Замените на ваш токен
 PASSWORD = "123444"           # Пароль для доступа к управлению
